d8ed326ad5c51bff5cd5b02fe7d5b24dce13df1b
jbixon13 <jbixon13@gmail.com>
2019-03-08 00:31:17 -0500

    add csv

--- sync_map/server.R
+++ sync_map/server.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(shiny)
 library(shinydashboard)
 library(leaflet)
@@ -6,17 +7,31 @@ library(DT)
 x <- c(1, 2, 3)
 y <- c(4, 5, 6)
 
+# load organizations csv  
+geo_data <- read_csv("geo_all.csv") 
+
 shinyServer(function(input, output) {
+    
+    # reactive expression for filtering map points
+    # filteredData <- reactive({
+    #     geo_data[geo_data$codes == input$codes]
+    # })
     
     # create base for leaflet map output
     output$map <- renderLeaflet({
         
         leaflet() %>% 
-            addTiles() %>% 
+            addProviderTiles(provider = providers$OpenStreetMap.BlackAndWhite) %>% 
             setView(lng = -76.62, lat = 39.29, zoom = 12) 
             
     })
     
+    # add parts of the map that will change in their own observers
+    # observe({
+    #     leafletProxy('map', data = filteredData())
+    # })
+    
+
     # create data table
     output$table <- renderDataTable(DT::datatable({
         data <- data.frame(x, y)
