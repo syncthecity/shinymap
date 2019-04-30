67a2e049c2b555e11a22cbd16b50927d82bc5d50
Madhusudan Ravi <madhusudanravi@Madhusudans-MBP.fios-router.home>
2019-04-30 06:17:51 -0400

    updated to accomodate heroku changes, dep changes

--- app.R
+++ app.R
@@ -6,7 +6,8 @@ library(shiny);
 envport <- Sys.getenv("PORT")
 if (envport!="") {
     # this is required for heroku deployment. it appears that they coerce the port on their end
-    runApp("sync_map", port=Sys.getenv(envport), host = getOption("shiny.host", "0.0.0.0"))
+    envportasint <- as.integer(envport)
+    runApp("sync_map", port=envportasint, host = getOption("shiny.host", "0.0.0.0"))
 } else {
     runApp("sync_map", port=3838, host = getOption("shiny.host", "0.0.0.0"))
 }
