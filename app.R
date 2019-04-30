4e904e9299ddb62875fe9bb19978371fd499882c
Madhusudan Ravi <madhusudanravi@Madhusudans-MBP.fios-router.home>
2019-04-29 21:37:31 -0400

    should address ticket #2 - heroku deploy of dockerized app

--- app.R
+++ app.R
@@ -3,4 +3,10 @@ packrat::on();
 require(shiny);
 library(shiny);
 
-runApp("sync_map", port=3838, host = getOption("shiny.host", "0.0.0.0"))
+envport <- Sys.getenv("PORT")
+if (envport!="") {
+    # this is required for heroku deployment. it appears that they coerce the port on their end
+    runApp("sync_map", port=Sys.getenv(envport), host = getOption("shiny.host", "0.0.0.0"))
+} else {
+    runApp("sync_map", port=3838, host = getOption("shiny.host", "0.0.0.0"))
+}
